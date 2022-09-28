dc3436c85eca4934c4a680e5e7f2cd50eed5fca2
iiDon <abu-farris@hotmail.com>
2022-09-29 00:41:57 +0300

    Fixed Error

--- src/pages/api/todos.ts
+++ src/pages/api/todos.ts
@@ -3,10 +3,10 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../server/db/client";
 
 const todos = async (req: NextApiRequest, res: NextApiResponse) => {
-  const todos = await prisma.todo.findMany()
-  if (todos) {
-    return res.status(200).json(todos);
-  }
+  // const todos = await prisma.todo.findMany()
+  // if (todos) {
+  //   return res.status(200).json(todos);
+  // }
   return res.status(200).json("No Todos")
 };
 
